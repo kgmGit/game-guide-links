088f9f2ebbbc33c1e6a4772a7c165613e3048da6
kgmGit <keigo005@gmail.com>
2021-12-01 19:26:10 +0900

    config:cacheした状態でパスワードリセットメールのURLがおかしくなるのを修正

--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -27,7 +27,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         ResetPassword::createUrlUsing(function ($user, string $token) {
-            return env('SPA_URL') . '/reset-password?token=' . $token;
+            return config('app.url') . '/reset-password?token=' . $token;
         });
     }
 }
